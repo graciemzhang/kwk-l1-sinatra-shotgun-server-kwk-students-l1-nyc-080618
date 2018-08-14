7f9ddfac2346b342632148e18b7c6807b6719113
graciemzhang <gracie.zhang@lps-students.org>
2018-08-14 18:48:09 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do 
-    "My name is Gracie & my favorite color is yellow"
+    "My name is Gracie & my favorite color is yellow and i love lordeeee"
   end
 
 end
